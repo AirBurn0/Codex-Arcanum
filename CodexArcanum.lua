3a7842e2f09423682d8937ae072b276b334340f2
AirBurn <rosan1233456@gmail.com>
2025-02-18 14:25:04 +0300

    migrate mod steamodded data to .json

--- CodexArcanum.lua
+++ CodexArcanum.lua
@@ -1,15 +1,3 @@
---- STEAMODDED HEADER
---- MOD_NAME: Codex Arcanum
---- MOD_ID: CodexArcanum
---- PREFIX: alchemy
---- MOD_AUTHOR: [itayfeder, Lyman, Jumbo, lshtech, AirBurn]
---- MOD_DESCRIPTION: Adds a new set of cards: Alchemy!
---- BADGE_COLOUR: C09D75
---- PRIORITY: 1000
---- VERSION: 1.1.5
---- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-1401a]
-----------------------------------------------
-------------MOD CODE -------------------------
 G.C.SECONDARY_SET.Alchemy = HEX("C09D75")
 
 SMODS.Atlas{ key = "modicon", px = 32, py = 32, path = "modicon.png" }
@@ -23,5 +11,3 @@ SMODS.load_file("data/CA_Decks.lua")()
 SMODS.load_file("data/CA_Jokers.lua")()
 SMODS.load_file("data/CA_Vouchers.lua")()
 SMODS.load_file("data/CA_Others.lua")()
-----------------------------------------------
-------------MOD CODE END----------------------
